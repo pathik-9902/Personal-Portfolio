06e2be5ba721050e093ffe35ac8ae6edd32699a6
Kratos <pathik.patel992002@gmail.com>
2024-05-18 21:01:53 +0530

    minor bug fixes

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-      anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-  
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-        });
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
       });
     });
   });
+});
 
 
+const sections = document.querySelectorAll('.animated-section');
 
-  
-  const sections = document.querySelectorAll('.animated-section');
-
-  const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('in-view');
-        observer.unobserve(entry.target);
-      }
-    });
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('in-view');
+    } else {
+      entry.target.classList.remove('in-view');
+    }
   });
-  
-  sections.forEach(section => {
-    observer.observe(section);
-  });
-  
+});
+
+sections.forEach(section => {
+  observer.observe(section);
+});
