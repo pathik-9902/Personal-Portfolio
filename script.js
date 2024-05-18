6b3926895216eba603c0d30890b2eb63f4089524
Kratos <pathik.patel992002@gmail.com>
2024-05-18 19:04:56 +0530

    animations added

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Smooth scrolling
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -11,3 +10,21 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
+
+
+  
+  const sections = document.querySelectorAll('.animated-section');
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('in-view');
+        observer.unobserve(entry.target);
+      }
+    });
+  });
+  
+  sections.forEach(section => {
+    observer.observe(section);
+  });
+  
