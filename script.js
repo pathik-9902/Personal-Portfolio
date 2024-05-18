d0e274475a774657c2ec8657aa7232bbc5cdf7f3
Kratos <pathik.patel992002@gmail.com>
2024-05-18 18:19:17 +0530

    Added many features and Updated CV

--- script.js
+++ script.js
@@ -10,18 +10,4 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     });
   });
-  
-  // Add this inside your existing DOMContentLoaded event listener
-window.addEventListener('scroll', function () {
-  var scrollButton = document.getElementById('scroll-to-top');
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    scrollButton.style.display = 'block';
-  } else {
-    scrollButton.style.display = 'none';
-  }
-});
 
-document.getElementById('scroll-to-top').addEventListener('click', function () {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-});
